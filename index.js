b2228f52e21a4e1a07d25054ead3ff2fd7b8acf1
Ritik Raj Pandey <ritikphenomenon@gmail.com>
2024-06-22 23:46:30 +0530

    cors issues

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); 
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -6,12 +6,9 @@ const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
 
-
-
 const app = express();
 
-
-//Ensure the directory for screenshots exists
+// Ensure the directory for screenshots exists
 const screenshotsDir = path.join(__dirname, 'screenshots');
 if (!fs.existsSync(screenshotsDir)) {
   fs.mkdirSync(screenshotsDir);
@@ -20,7 +17,23 @@ if (!fs.existsSync(screenshotsDir)) {
 app.use('/screenshots', express.static(path.join(__dirname, 'screenshots')));
 
 app.use(bodyParser.json());
-app.use(cors());
+
+// Configure CORS
+const allowedOrigins = [
+  'https://web-scrapping-5sw8mahjj-ritikphenomenons-projects.vercel.app', // Add your allowed origins here
+  'http://localhost:5173' // Add localhost for testing purposes
+];
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // Routes
 const companyRoutes = require('./routes/companyRoutes');
@@ -32,7 +45,8 @@ const PORT = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.l7qhl3q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose.connect(uri, {
- 
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 }).then(() => {
   console.log('Database connected successfully');
   app.listen(PORT, () => {
@@ -41,4 +55,3 @@ mongoose.connect(uri, {
 }).catch((error) => {
   console.error('Database connection failed:', error);
 });
-
